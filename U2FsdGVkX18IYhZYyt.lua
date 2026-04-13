df802aa73e8eb6b1abd7c780f6f59d5c8f32e13b
activednow1 <activednow1@gmail.com>
2026-04-13 18:22:11 +0700

    Update U2FsdGVkX18IYhZYyt.lua

--- U2FsdGVkX18IYhZYyt.lua
+++ U2FsdGVkX18IYhZYyt.lua
@@ -1,10 +1,65 @@
 script_key="TNKqRPVddeXbUfXarbqmEKcjEsrgfNYh";
-setfpscap(10)
+setfpscap(25)
 
 getgenv().sailorPieceConfig = {  
-    OPTIMIZATION = true,  -- true = optimize + show ui, false = disable ui
-    AUTO_KICK = true,  -- Autokick if no TRADE_ITEM items left
+    AUTO_UPDATE_RESTART = true,
+    ASCEND_UNTIL_LEVEL = 5,
+    
+    -- Autofarm
+    DO_REPEATABLE_QUEST = "QuestNPC19",
+    AUTO_FARM = { "IceQueenBoss", "ArenaFighter", "Ninja", "SlimeWarior", "Swordsman", "Quincy", "AcademyTeacher", "Slime", "Curse", "StrongSorcerer", "AtomicBoss", "MoonSlayerBoss", "StrongestinHistoryBoss", "Gilgamesh", "RimuruBoss", "StrongestShinobiBoss", "TrueAizenBoss", "Aizen" },  -- Auto farm ascend/quest/weapon bosses
+    SUMMON_BOSS = { "Ice Queen Normal", "Gilgamesh Normal", "Moon Slayer Normal", "Saber Normal", "Saber Normal" },  -- Auto summon ascend/quest/weapon bosses
 
+
+    -- Weapons
+    EQUIP_WEAPON = {"Ice Queen", "Strongest Shinobi", "Moon Slayer", "Saber Alter", "Gilgamesh", "Strongest In History", "Ichigo", "Gryphon", "Dark Blade", "Katana"},
+    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Gilgamesh", "Saber Alter", "Moon Slayer", "Strongest Shinobi", "Ice Queen"},
+    BLESS_WEAPON = { 
+      ["Ice Queen"] = 10, 
+      ["Strongest Shinobi"] = 10, 
+      ["Moon Slayer"] = 10, 
+      ["Saber Alter"] = 10, 
+      ["Gilgamesh"] = 10, 
+      ["Strongest In History"] = 10, 
+      ["Ichigo"] = 10, 
+      ["Gryphon"] = 6, 
+      ["Dark Blade"] = 3 
+    },
+
+    -- Reroll
+    REROLL_RACE_UNTIL = { "Luckborn", "Warlord", "Sunborn", "Servant", "Kitsune", "Swordblessed", "Galevorn", "Leviathan", "Oni" },
+    REROLL_CLAN_UNTIL = { "Frostbane", "Monarch", "Eminence" },
+    REROLL_TRAIT_UNTIL = { "Overlord", "Cataclysm", "Singularity", "Celestial", "Emperor" },
+    REROLL_STAT_UNTIL = { 
+      ["Damage"] = "Z", 
+      ["Defense"] = "S", 
+      ["CooldownReduction"] = "S", 
+      ["CritChance"] = "S", 
+      ["CritDamage"] = "S", 
+      ["DamageReduction"] = "S", 
+      ["Luck"] = "Z" 
+    },
+    REROLL_PASSIVE_UNTIL = { 
+      ["Ichigo"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" }, 
+      ["Strongest In History"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" },
+      ["Gilgamesh"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" },
+      ["Saber Alter"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" },
+      ["Moon Slayer"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" },
+      ["Strongest Shinobi"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" },
+      ["Ice Queen"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V" },
+    },
+    REROLL_POWER_UNTIL = { "Colossus" },
+
+    -- Artifact
+    DELETE_ARTIFACT_RARITY = { "Common" },
+    EQUIP_ARTIFACT_SET = "Abyssal Crown",
+
+    -- Misc
+    BUILD_MODE = "Damage",  -- Damage/Luck
+    USE_ITEM = { "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Aura Crate (Untradeable)", "Cosmetic Crate (Untradeable)", "Secret Chest (Untradeable)" },
+    BUY_MERCHANT = { "Race Reroll", "Trait Reroll", "Clan Reroll", "Passive Shard", "Boss Key", "Dungeon Key", "Rush Key", "Boss Ticket", "Cosmetic Crate", "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Mythical Chest", "Secret Chest", "Aura Crate" },
+
+    -- Autotrade
     TRADE_USERNAME = { "petanimaximal14", "petanimaximal15", "petanimaximal17", "petanimaximal18", "petanimaximal19", "petanimaximal120", "petanimaximal21", "petanimaximal22", "petanimaximal23", "petanimaximal24", "petanimaximal25", "petanimaximal26", "petanimaximal27", "petanimaximal28" },  -- "Username"
     TRADE_ITEM = {
 
@@ -126,11 +181,14 @@ getgenv().sailorPieceConfig = {
 
 },
 
-    WEBHOOK_URL = "",
+    -- Discord
+    WEBHOOK_ITEM_NAME = { "Aura Crate", "Cosmetic Crate", "Secret Chest" },
+    WEBHOOK_URL = "https://discordapp.com/api/webhooks/1491133044751466568/AFqJC6CFIH1Lc96BUCrsXOuyqTsG5a-syDa9fx1bsc4FH9h01rdG81SlOk5drg3rCmDH",
     DISCORD_ID = "",
-    WEBHOOK_NOTE = "vps1",
+    WEBHOOK_NOTE = "",
+    SHOW_PUBLIC_DISCORD_ID = true,
     SHOW_WEBHOOK_USERNAME = true,
     SHOW_WEBHOOK_JOBID = true,
-}
+}   
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/eb9a467b35fe098d20677eb16ec559a4.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c7ac2a2f86ecf894218a424a1be7667.lua"))()
