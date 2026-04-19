4d026fbecad909771903bd48f314f847208d4ec9
activednow1 <activednow1@gmail.com>
2026-04-19 10:19:54 +0700

    Update U2FsdGVkX18IYhZYyt.lua

--- U2FsdGVkX18IYhZYyt.lua
+++ U2FsdGVkX18IYhZYyt.lua
@@ -1,31 +1,24 @@
 script_key="TNKqRPVddeXbUfXarbqmEKcjEsrgfNYh";
-setfpscap(30)
+setfpscap(20)
 
 getgenv().sailorPieceConfig = {  
     AUTO_UPDATE_RESTART = true,
-    ASCEND_UNTIL_LEVEL = 5,
+    ASCEND_UNTIL_LEVEL = 10,
+    AUTO_CELESTIAL_FAVOR_TITLE = true, -- Auto attack 5000 island bosses
+    AUTO_BOSS_RUSH_AND_INFINITE_TOWER_STAT_BONUS = false,
     
     -- Autofarm
     DO_REPEATABLE_QUEST = "QuestNPC19",
-    MULTI_FARM = { "ArenaFighter", "Ninja", "Swordsman", "AcademyTeacher", "Slime", "StrongSorcerer", "Curse", "Hollow", "Sorcerer", "FrostRogue", "DesertMonkey", "Monkey", "Thief" },  -- Instant tp kill npc (Must have Strongest In History)
+    -- MULTI_FARM -> Instant tp kill npc (Must have Strongest In History/Ichigo/Gryphon)
+    MULTI_FARM = { "Bunny", "ArenaFighter", "Ninja", "Swordsman", "AcademyTeacher", "Slime", "StrongSorcerer", "Curse", "Hollow", "Sorcerer", "FrostRogue", "DesertMonkey", "Monkey", "Thief" },  
     AUTO_FARM = { "JinwooBoss", "YujiBoss", "GojoBoss", "SukunaBoss", "AizenBoss", "SaberBoss", "QinShiBoss", "IchigoBoss", "GilgameshBoss", "BlessedMaidenBoss", "SaberAlterBoss", "MoonSlayerBoss", "IceQueenBoss", "StrongestofTodayBoss", "StrongestinHistoryBoss", "RimuruBoss", "AnosBoss", "YamatoBoss", "TrueAizenBoss", "StrongestShinobiBoss", "AtomicBoss", "SoulReaperBoss", "Soul Reaper",  },  -- Auto farm ascend/quest/weapon bosses
     SUMMON_BOSS = { "Ice Queen Normal", "Soul Reaper Normal", "Soul Reaper", "Moon Slayer Normal", "Ichigo Normal", "Saber Normal", "AtomicBoss Normal", "Atomic Normal", "Saber Alter Normal", "SaberAlter Normal", "True Aizen Normal" , "TrueAizen Normal", "Strongest in History Normal", "StrongestinHistory Normal", "Strongest of Today Normal", "StrongestofToday Normal", "True Manipulator Normal" },  -- Auto summon ascend/quest/weapon bosses
 
-
     -- Weapons
+    -- Script will auto equip (Ice Queen + Damage) build for dungeon/inf tower
     EQUIP_WEAPON = {"Strongest In History", "Ichigo", "Gryphon", "Dark Blade", "Katana"},
-    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Gilgamesh", "Saber Alter", "Moon Slayer", "Strongest Shinobi", "Ice Queen"},
-    BLESS_WEAPON = { 
-      ["Ice Queen"] = 10, 
-      ["Strongest Shinobi"] = 10, 
-      ["Moon Slayer"] = 10, 
-      ["Saber Alter"] = 10, 
-      ["Gilgamesh"] = 10, 
-      ["Strongest In History"] = 10, 
-      ["Ichigo"] = 10, 
-      ["Gryphon"] = 6, 
-      ["Dark Blade"] = 3 
-    },
+    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Ice Queen"},
+    BLESS_WEAPON = { ["Ice Queen"] = 10, ["Strongest In History"] = 10, ["Ichigo"] = 6, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
 
     -- Reroll
     REROLL_RACE_UNTIL = { "Luckborn" },
@@ -45,7 +38,7 @@ getgenv().sailorPieceConfig = {
         ["Strongest In History"] = { "Fortune Chosen" }, 
         ["Ichigo"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V", "Damage IV" } 
     },
-    REROLL_POWER_UNTIL = { "Colossus" },
+    REROLL_POWER_UNTIL = { "Subjugator" },
 
     -- Artifact
     DELETE_ARTIFACT_RARITY = { "Common" },
@@ -57,127 +50,9 @@ getgenv().sailorPieceConfig = {
     BUY_MERCHANT = { "Race Reroll", "Trait Reroll", "Clan Reroll", "Passive Shard", "Boss Key", "Dungeon Key", "Rush Key", "Boss Ticket", "Cosmetic Crate", "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Mythical Chest", "Secret Chest", "Aura Crate" },
 
     -- Autotrade
-    TRADE_USERNAME = { "petanimaximal14", "petanimaximal15", "petanimaximal17", "petanimaximal18", "petanimaximal19", "petanimaximal120", "petanimaximal21", "petanimaximal22", "petanimaximal23", "petanimaximal24", "petanimaximal25", "petanimaximal26", "petanimaximal27", "petanimaximal28" },  -- "Username"
-    TRADE_ITEM = {
-
-    "Abyss Edge",
-
-    "Battle Shard",
-
-    "Boss Key",
-
-    "Boss Ticket",
-
-    "Broken Sword",
-
-    "Cursed Finger",
-
-    "Dungeon Key",
-
-    "Flash Impact",
-        
-    "Illusion Prism",
-
-    "Limitless Ring",
-
-    "Mirage Pendant",
-
-    "Obsidian",
-
-    "Race Reroll",
-
-    "Soul Fragment",
-
-    "Trait Reroll",
-
-    "Umbral Capsule",
-
-    "Void Fragment",
-
-    "Worthiness Fragment",
-
-    "Abyss Sigil",
-
-    "Ancient Shard",
-
-    "Chrysalis Sigil",
-
-    "Clan Reroll",
-
-    "Dark Grail",
-
-    "Dark Ring",
-
-    "Dismantle Fang",
-
-    "Divine Grail",
-
-    "Frost Relic",
-
-    "Glacier Remnant",
-
-    "Golden Essence",
-
-    "Infinity Core",
-
-    "Mythril",
-
-    "Passive Shard",
-
-    "Power Shard",
-
-    "Reiatsu Core",
-
-    "Rush Key",
-
-    "Tempest Relic",
-
-    "Tower Key",
-
-    "Upper Seal",
-
-    "Void Seed",
-
-    "Adamantite",
-
-    "Atomic Core",
-
-    "Blood Ring",
-
-    "Crimson Heart",
-
-    "Diamond",
-
-    "Frozen Brand",
-
-    "Hōgyoku Fragment",
-
-    "Hogyoku Fragment",
-
-    "Ice Core",
-
-    "Imperial Seal",
-
-    "Mythical Chest",
-
-    "Phantasm Core",
-
-    "Shadow Heart",
-
-    "Soul Flame",
-
-    "Transmutation Shard",
-
-    "Iron",
-
-    "Aura Crate",
-
-    "Cosmetic Crate",
-
-    "Secret Chest",
-
-},
-
+    TRADE_USERNAME = {},
+    TRADE_ITEM = {},
+    
     -- Discord
     WEBHOOK_ITEM_NAME = { "Aura Crate", "Cosmetic Crate", "Secret Chest" },
     WEBHOOK_URL = "https://discordapp.com/api/webhooks/1491133044751466568/AFqJC6CFIH1Lc96BUCrsXOuyqTsG5a-syDa9fx1bsc4FH9h01rdG81SlOk5drg3rCmDH",
