ddcb2361fd736e6ae6113c962812adf6efea7631
activednow1 <activednow1@gmail.com>
2026-04-20 04:51:53 +0700

    Update U2FsdGVkX18IYhZYyt.lua

--- U2FsdGVkX18IYhZYyt.lua
+++ U2FsdGVkX18IYhZYyt.lua
@@ -1,66 +1,149 @@
 script_key="TNKqRPVddeXbUfXarbqmEKcjEsrgfNYh";
-setfpscap(20)
+setfpscap(10)
 
 getgenv().sailorPieceConfig = {  
-    AUTO_UPDATE_RESTART = true,
-    ASCEND_UNTIL_LEVEL = 10,
-    AUTO_CELESTIAL_FAVOR_TITLE = true, -- Auto attack 5000 island bosses
-    AUTO_BOSS_RUSH_AND_INFINITE_TOWER_STAT_BONUS = false,
+    OPTIMIZATION = true,  -- true = optimize + show ui, false = disable ui
+    AUTO_KICK = true,  -- Autokick if no TRADE_ITEM items left
+   TRADE_USERNAME = { "petanimaximal14", "petanimaximal15", "petanimaximal17", "petanimaximal18", "petanimaximal19", "petanimaximal120", "petanimaximal21", "petanimaximal22", "petanimaximal23", "petanimaximal24", "petanimaximal25", "petanimaximal26", "petanimaximal27", "petanimaximal28" },  -- "Username"
+    TRADE_ITEM = {
+
+    "Abyss Edge",
+
+    "Battle Shard",
+
+    "Easter Egg",
+
+    "Lunar Essence", 
+
+    "Blue Singularity",
+
+    "Yamato Essence",
+
+    "Atomic Omen",
+
+    "Corrupt Crown",
+
+    "Corruption Core",
+
+    "Crescent Shard",
+
+    "Transcendent Core",
+
+    "Fusion Ring",
+
+    "Divinity Essence",
     
-    -- Autofarm
-    DO_REPEATABLE_QUEST = "QuestNPC19",
-    -- MULTI_FARM -> Instant tp kill npc (Must have Strongest In History/Ichigo/Gryphon)
-    MULTI_FARM = { "Bunny", "ArenaFighter", "Ninja", "Swordsman", "AcademyTeacher", "Slime", "StrongSorcerer", "Curse", "Hollow", "Sorcerer", "FrostRogue", "DesertMonkey", "Monkey", "Thief" },  
-    AUTO_FARM = { "JinwooBoss", "YujiBoss", "GojoBoss", "SukunaBoss", "AizenBoss", "SaberBoss", "QinShiBoss", "IchigoBoss", "GilgameshBoss", "BlessedMaidenBoss", "SaberAlterBoss", "MoonSlayerBoss", "IceQueenBoss", "StrongestofTodayBoss", "StrongestinHistoryBoss", "RimuruBoss", "AnosBoss", "YamatoBoss", "TrueAizenBoss", "StrongestShinobiBoss", "AtomicBoss", "SoulReaperBoss", "Soul Reaper",  },  -- Auto farm ascend/quest/weapon bosses
-    SUMMON_BOSS = { "Ice Queen Normal", "Soul Reaper Normal", "Soul Reaper", "Moon Slayer Normal", "Ichigo Normal", "Saber Normal", "AtomicBoss Normal", "Atomic Normal", "Saber Alter Normal", "SaberAlter Normal", "True Aizen Normal" , "TrueAizen Normal", "Strongest in History Normal", "StrongestinHistory Normal", "Strongest of Today Normal", "StrongestofToday Normal", "True Manipulator Normal" },  -- Auto summon ascend/quest/weapon bosses
+    "Evolution Fragment",
 
-    -- Weapons
-    -- Script will auto equip (Ice Queen + Damage) build for dungeon/inf tower
-    EQUIP_WEAPON = {"Strongest In History", "Ichigo", "Gryphon", "Dark Blade", "Katana"},
-    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Ice Queen"},
-    BLESS_WEAPON = { ["Ice Queen"] = 10, ["Strongest In History"] = 10, ["Ichigo"] = 6, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
+    "Broken Sword",
 
-    -- Reroll
-    REROLL_RACE_UNTIL = { "Luckborn" },
-    REROLL_CLAN_UNTIL = { "Eminence" },
-    REROLL_TRAIT_UNTIL = { "Emperor" },
-    REROLL_STAT_UNTIL = { 
-        ["Damage"] = "Z", 
-        ["Defense"] = "SS", 
-        ["CooldownReduction"] = "Z", 
-        ["CritChance"] = "SSS", 
-        ["CritDamage"] = "SSS", 
-        ["DamageReduction"] = "SS", 
-        ["Luck"] = "Z" 
-    },
-    REROLL_PASSIVE_UNTIL = { 
-        ["Ice Queen"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
-        ["Strongest In History"] = { "Fortune Chosen" }, 
-        ["Ichigo"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V", "Damage IV" } 
-    },
-    REROLL_POWER_UNTIL = { "Subjugator" },
+    "Cursed Finger",
 
-    -- Artifact
-    DELETE_ARTIFACT_RARITY = { "Common" },
-    EQUIP_ARTIFACT_SET = "Abyssal Crown",
+    "Flash Impact",
+        
+    "Illusion Prism",
 
-    -- Misc
-    BUILD_MODE = "Luck",  -- Damage/Luck
-    USE_ITEM = { "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Aura Crate (Untradeable)", "Cosmetic Crate (Untradeable)", "Secret Chest (Untradeable)" },
-    BUY_MERCHANT = { "Race Reroll", "Trait Reroll", "Clan Reroll", "Passive Shard", "Boss Key", "Dungeon Key", "Rush Key", "Boss Ticket", "Cosmetic Crate", "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Mythical Chest", "Secret Chest", "Aura Crate" },
+    "Limitless Ring",
 
-    -- Autotrade
-    TRADE_USERNAME = {},
-    TRADE_ITEM = {},
-    
-    -- Discord
-    WEBHOOK_ITEM_NAME = { "Aura Crate", "Cosmetic Crate", "Secret Chest" },
-    WEBHOOK_URL = "https://discordapp.com/api/webhooks/1491133044751466568/AFqJC6CFIH1Lc96BUCrsXOuyqTsG5a-syDa9fx1bsc4FH9h01rdG81SlOk5drg3rCmDH",
+    "Mirage Pendant",
+
+    "Obsidian",
+
+    "Race Reroll",
+
+    "Soul Fragment",
+
+    "Trait Reroll",
+
+    "Umbral Capsule",
+
+    "Void Fragment",
+
+    "Worthiness Fragment",
+
+    "Abyss Sigil",
+
+    "Ancient Shard",
+
+    "Chrysalis Sigil",
+
+    "Clan Reroll",
+
+    "Dark Grail",
+
+    "Dark Ring",
+
+    "Dismantle Fang",
+
+    "Divine Grail",
+
+    "Frost Relic",
+
+    "Glacier Remnant",
+
+    "Golden Essence",
+
+    "Infinity Core",
+
+    "Mythril",
+
+    "Passive Shard",
+
+    "Power Shard",
+
+    "Reiatsu Core",
+
+    "Tempest Relic",
+
+    "Upper Seal",
+
+    "Void Seed",
+
+    "Adamantite",
+
+    "Atomic Core",
+
+    "Blood Ring",
+
+    "Crimson Heart",
+
+    "Diamond",
+
+    "Frozen Brand",
+
+    "Hōgyoku Fragment",
+
+    "Hogyoku Fragment",
+
+    "Ice Core",
+
+    "Imperial Seal",
+
+    "Mythical Chest",
+
+    "Phantasm Core",
+
+    "Shadow Heart",
+
+    "Soul Flame",
+
+    "Transmutation Shard",
+
+    "Iron",
+
+    "Aura Crate",
+
+    "Cosmetic Crate",
+
+    "Secret Chest",
+
+},
+
+    WEBHOOK_URL = "",
     DISCORD_ID = "",
-    WEBHOOK_NOTE = "",
-    SHOW_PUBLIC_DISCORD_ID = true,
+    WEBHOOK_NOTE = "vps1",
     SHOW_WEBHOOK_USERNAME = true,
     SHOW_WEBHOOK_JOBID = true,
-}   
+}
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c7ac2a2f86ecf894218a424a1be7667.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/eb9a467b35fe098d20677eb16ec559a4.lua"))()
