aa12aa8cbe43ca5efb9635f7aa9dada9249d89f5
activednow1 <activednow1@gmail.com>
2026-04-20 11:16:59 +0700

    Update U2FsdGVkX18IYhZYyt.lua

--- U2FsdGVkX18IYhZYyt.lua
+++ U2FsdGVkX18IYhZYyt.lua
@@ -2,158 +2,67 @@ script_key="TNKqRPVddeXbUfXarbqmEKcjEsrgfNYh";
 setfpscap(10)
 
 getgenv().sailorPieceConfig = {  
-    OPTIMIZATION = true,  -- true = optimize + show ui, false = disable ui
-    AUTO_KICK = true,  -- Autokick if no TRADE_ITEM items left
-   TRADE_USERNAME = { "petanimaximal14", "petanimaximal15", "petanimaximal17", "petanimaximal18", "petanimaximal19", "petanimaximal120", "petanimaximal21", "petanimaximal22", "petanimaximal23", "petanimaximal24", "petanimaximal25", "petanimaximal26", "petanimaximal27", "petanimaximal28" },  -- "Username"
-    TRADE_ITEM = {
-
-    "Abyss Edge",
-
-    "Boss Key",
-
-    "Boss Ticket",
-
-    "Dungeon Key",
-
-    "Rush Key",
-
-    "Tower Key",
-
-    "Battle Shard",
-
-    "Easter Egg",
-
-    "Lunar Essence", 
-
-    "Blue Singularity",
-
-    "Yamato Essence",
-
-    "Atomic Omen",
-
-    "Corrupt Crown",
-
-    "Corruption Core",
-
-    "Crescent Shard",
-
-    "Transcendent Core",
-
-    "Fusion Ring",
-
-    "Divinity Essence",
-
-    "Evolution Fragment",
-
-    "Broken Sword",
-
-    "Cursed Finger",
-
-    "Flash Impact",
-
-    "Illusion Prism",
-
-    "Limitless Ring",
-
-    "Mirage Pendant",
-
-    "Obsidian",
-
-    "Race Reroll",
-
-    "Soul Fragment",
-
-    "Trait Reroll",
-
-    "Umbral Capsule",
-
-    "Void Fragment",
-
-    "Worthiness Fragment",
-
-    "Abyss Sigil",
-
-    "Ancient Shard",
-
-    "Chrysalis Sigil",
-
-    "Clan Reroll",
-
-    "Dark Grail",
-
-    "Dark Ring",
-
-    "Dismantle Fang",
-
-    "Divine Grail",
-
-    "Frost Relic",
-
-    "Glacier Remnant",
-
-    "Golden Essence",
-
-    "Infinity Core",
-
-    "Mythril",
-
-    "Passive Shard",
-
-    "Power Shard",
-
-    "Reiatsu Core",
-
-    "Tempest Relic",
-
-    "Upper Seal",
-
-    "Void Seed",
-
-    "Adamantite",
-
-    "Atomic Core",
-
-    "Blood Ring",
-
-    "Crimson Heart",
-
-    "Diamond",
-
-    "Frozen Brand",
-
-    "Hōgyoku Fragment",
-
-    "Hogyoku Fragment",
-
-    "Ice Core",
-
-    "Imperial Seal",
-
-    "Mythical Chest",
-
-    "Phantasm Core",
-
-    "Shadow Heart",
-
-    "Soul Flame",
-
-    "Transmutation Shard",
-
-    "Iron",
-
-    "Aura Crate",
-
-    "Cosmetic Crate",
-
-    "Secret Chest",
-
-},
-
+    AUTO_UPDATE_RESTART = true,
+    WORLD = "Sea 1",  -- "Sea 1", "Sea 2"
+    ASCEND_UNTIL_LEVEL = 10,
+    AUTO_CELESTIAL_FAVOR_TITLE = true, -- Auto attack 5000 island bosses
+    AUTO_BOSS_RUSH_AND_INFINITE_TOWER_STAT_BONUS = false,
+    
+    -- Autofarm
+    DO_REPEATABLE_QUEST = "QuestNPC19",
+    -- MULTI_FARM -> Instant tp kill npc (Must have Strongest In History/Ichigo/Gryphon)
+    MULTI_FARM = { "Bunny", "ArenaFighter", "Ninja", "Swordsman", "AcademyTeacher", "Slime", "StrongSorcerer", "Curse", "Hollow", "Sorcerer", "FrostRogue", "DesertBandit", "Monkey", "Thief" },  
+    AUTO_FARM = {},  -- Auto farm ascend/quest/weapon bosses 
+    SUMMON_BOSS = {},  -- Auto summon ascend/quest/weapon bosses
+    
+    -- Weapons
+    -- Script will auto equip (Ice Queen + Damage) build for dungeon/inf tower
+    EQUIP_WEAPON = {"Strongest In History", "Ichigo", "Gryphon", "Dark Blade", "Katana"},
+    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Ice Queen"},
+    BLESS_WEAPON = { ["Ice Queen"] = 10, ["Strongest In History"] = 10, ["Ichigo"] = 6, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
+
+    -- Reroll
+    REROLL_RACE_UNTIL = { "Luckborn" },
+    REROLL_CLAN_UNTIL = { "Eminence" },
+    REROLL_TRAIT_UNTIL = { "Emperor" },
+    REROLL_STAT_UNTIL = { 
+        ["Damage"] = "Z", 
+        ["Defense"] = "SS", 
+        ["CooldownReduction"] = "Z", 
+        ["CritChance"] = "SSS", 
+        ["CritDamage"] = "SSS", 
+        ["DamageReduction"] = "SS", 
+        ["Luck"] = "Z" 
+    },
+    REROLL_PASSIVE_UNTIL = { 
+        ["Ice Queen"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
+        ["Strongest In History"] = { "Fortune Chosen" }, 
+        ["Ichigo"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V", "Damage IV" } 
+    },
+    REROLL_POWER_UNTIL = { "Subjugator" },
+    REROLL_BLOODLINE_UNTIL = { "Cosmic" },
+
+    -- Artifact
+    DELETE_ARTIFACT_RARITY = { "Common" },
+    EQUIP_ARTIFACT_SET = "Abyssal Crown",
+
+    -- Misc
+    BUILD_MODE = "Luck",  -- Damage/Luck
+    USE_ITEM = { "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Aura Crate (Untradeable)", "Cosmetic Crate (Untradeable)", "Secret Chest (Untradeable)" },
+    BUY_MERCHANT = { "Race Reroll", "Trait Reroll", "Clan Reroll", "Passive Shard", "Boss Key", "Dungeon Key", "Rush Key", "Boss Ticket", "Haki Color Reroll", "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Mythical Chest", "Secret Chest" },
+
+    -- Autotrade
+    TRADE_USERNAME = {},
+    TRADE_ITEM = {},
+    
+    -- Discord
+    WEBHOOK_ITEM_NAME = { "Aura Crate", "Cosmetic Crate", "Secret Chest" },
     WEBHOOK_URL = "",
     DISCORD_ID = "",
-    WEBHOOK_NOTE = "vps1",
+    WEBHOOK_NOTE = "",
+    SHOW_PUBLIC_DISCORD_ID = true,
     SHOW_WEBHOOK_USERNAME = true,
     SHOW_WEBHOOK_JOBID = true,
 }
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/eb9a467b35fe098d20677eb16ec559a4.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c7ac2a2f86ecf894218a424a1be7667.lua"))()
