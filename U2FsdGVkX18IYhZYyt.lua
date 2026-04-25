9b75edca099f7ddd49960f0505a7ed8979bae284
activednow1 <activednow1@gmail.com>
2026-04-25 22:33:29 +0700

    Update U2FsdGVkX18IYhZYyt.lua

--- U2FsdGVkX18IYhZYyt.lua
+++ U2FsdGVkX18IYhZYyt.lua
@@ -1,157 +1,69 @@
 script_key="TNKqRPVddeXbUfXarbqmEKcjEsrgfNYh";
-setfpscap(10)
+setfpscap(20)
+
 
 getgenv().sailorPieceConfig = {  
-    OPTIMIZATION = true,  -- true = optimize + show ui, false = disable ui
-    AUTO_KICK = true,  -- Autokick if no TRADE_ITEM items left
-    KICK_IF_NO_TRADE_USERNAME = false,
-    TRADE_SEA_1 = true,
-   TRADE_USERNAME = { "petanimaximal30", "petanimaximal31", "petanimaximal32", "petanimaximal33", "petanimaximal34", "petanimaximal35" },  -- "Username"
-    TRADE_ITEM = {
-
-    "Aura Crate",
-
-    "Cosmetic Crate",
-
-    "Secret Chest",
-
-    "Limitless Ring",
-
-    "Battle Shard",
-
-    "Cosmic Essence",
-
-    "Cursed Finger", 
-
-    "Power Fragment",
-
-    "Void Fragment",
-
-    "Worthiness Fragment",
-
-    "Abyss Sigil",
-
-    "Alter Essence",
-
-    "Battle Sigil",
-
-    "Bloodline Stone",
-
-    "Easter Egg",
-
-    "Chrysalis Sigil",
-
-    "Clan Reroll",
-
-    "Dark Grail",
-
-    "Dark Ring",
-
-    "Divine Grail",
-
-    "Dominion Brand",
-
-    "Easter Key",
-
-    "Frost Relic",
-
-    "Fusion Ring",
-
-    "Glacier Remnant",
-
-    "Golden Essence",
-
-    "Lunar Essence",
-
-    "Passive Shard",
-
-    "Power Shard",
-
-    "Relic Part #1",
-
-    "Relic Part #2",
-
-    "Relic Part #3",
-
-    "Relic Part #4",
-
-    "Relic Part #5",
-
-    "Relic Part #6",
-
-    "Relic Part #7",
-
-    "Relic Part #8",
-
-    "Star Mark",
-
-    "Tempest Relic",
-
-    "Time Remnant",
-
-    "Upper Seal",
-
-    "Void Seed",
-
-    "Yamato Essence",
-
-    "Adamantite",
-
-    "Atomic Core",
-
-    "Blood Ring",
-
-    "Corruption Core",
-
-    "Corrupt Crown",
-
-    "Crescent Shard",
-
-    "Diamond",
-
-    "Frozen Brand",
-
-    "Galaxy Shard",
-
-    "Ice Core",
-
-    "Imperial Seal",
-
-    "Monster Pulse",
-
-    "Mythical Chest",
-
-    "Phantasm Core",
-
-    "Shadow Heart",
-
-    "Transmutation Shard",
-
-    "Vampire Omen",
-
-    "World Core",
-
-    "Guild Key",
-
-    "Throne Remnant",
-
-    "Evolution Fragment",
-
-    "Transcendent Core",
-
-    "Divinity Essence",
-
-    "Magic Shard",
-
-    "Atomic Omen",
-
-},
-
-    WEBHOOK_URL = "",
+    AUTO_UPDATE_RESTART = true,
+    ASCEND_UNTIL_LEVEL = 10,
+    WORLD = "Sea 2",
+    AUTO_CELESTIAL_FAVOR_TITLE = true, -- Auto attack 5000 island bosses
+    AUTO_BOSS_RUSH_AND_INFINITE_STAT_BONUS = false,
+ 
+    -- Autofarm
+    DO_REPEATABLE_QUEST = "QuestNPC23",
+    -- MULTI_FARM -> Instant tp kill npc (Must have Strongest In History/Ichigo/Gryphon)
+    MULTI_FARM = { "Bunny", "Quincy", "FastNinja", "StrongBandit", "StrongFighter", "Delinquent", "Bunny", "ArenaFighter", "Ninja", "Swordsman", "AcademyTeacher", "Slime", "StrongSorcerer", "Curse", "Hollow", "Sorcerer", "FrostRogue", "DesertBandit", "Monkey", "Thief" },  
+    AUTO_FARM = { "Great Mage Boss", "Cosmic Being", "The World Boss", "True Manipulator Boss", "Strongest Shinobi Boss", "Corrupted Knight Boss", "Yamato", "The World Boss", "Cosmic Being Boss", "TheWorldBoss", "CosmicBeingBoss" },  -- Auto farm ascend/quest/weapon bosses 
+    SUMMON_BOSS = { "Ice Queen Normal", "Soul Reaper Normal", "Soul Reaper", "Moon Slayer Normal", "Ichigo Normal", "Saber Normal", "AtomicBoss Normal", "Atomic Normal", "Saber Alter Normal", "SaberAlter Normal", "True Aizen Normal" , "TrueAizen Normal", "Strongest in History Normal", "StrongestinHistory Normal", "Strongest of Today Normal", "StrongestofToday Normal", "True Manipulator Normal" },  -- Auto summon ascend/quest/weapon bosses
+ 
+    -- Weapons
+    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Ice Queen" },
+    BLESS_WEAPON = { ["Cosmic Being"] = 10, ["The World"] = 10, ["Ice Queen"] = 10, ["Strongest In History"] = 10, ["Ichigo"] = 6, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
+ 
+    -- Reroll
+    REROLL_RACE_UNTIL = { "Luckborn" },
+    REROLL_CLAN_UNTIL = { "Eminence" },
+    REROLL_TRAIT_UNTIL = { "Emperor" },
+    REROLL_STAT_UNTIL = { 
+        ["Damage"] = "Z", 
+        ["Defense"] = "SS", 
+        ["CooldownReduction"] = "Z", 
+        ["CritChance"] = "SSS", 
+        ["CritDamage"] = "SSS", 
+        ["DamageReduction"] = "SS", 
+        ["Luck"] = "Z" 
+    },
+    REROLL_PASSIVE_UNTIL = { 
+        ["Cosmic Being"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
+        ["The World"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
+        ["Ice Queen"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
+        ["Strongest In History"] = { "Fortune Chosen" }, 
+        ["Ichigo"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V", "Damage IV" } 
+    },
+    REROLL_POWER_UNTIL = { "Subjugator" },
+    REROLL_BLOODLINE_UNTIL = { "Primordial" },
+ 
+    -- Artifact
+    DELETE_ARTIFACT_RARITY = { "Common" },
+    EQUIP_ARTIFACT_SET = "Abyssal Crown",
+ 
+    -- Misc
+    BUILD_MODE = "Luck",  -- Damage/Luck
+    USE_ITEM = { "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Aura Crate (Untradeable)", "Cosmetic Crate (Untradeable)", "Secret Chest (Untradeable)" },
+    BUY_MERCHANT = { "Race Reroll", "Trait Reroll", "Clan Reroll", "Passive Shard", "Boss Key", "Dungeon Key", "Rush Key", "Boss Ticket", "Haki Color Reroll", "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Mythical Chest", "Secret Chest" },
+
+    -- Autotrade
+    TRADE_USERNAME = {},
+    TRADE_ITEM = {},
+    
+    -- Discord
+    WEBHOOK_ITEM_NAME = { "Aura Crate", "Cosmetic Crate", "Secret Chest" },
+    WEBHOOK_URL = "https://discordapp.com/api/webhooks/1491133044751466568/AFqJC6CFIH1Lc96BUCrsXOuyqTsG5a-syDa9fx1bsc4FH9h01rdG81SlOk5drg3rCmDH",
     DISCORD_ID = "",
-    WEBHOOK_NOTE = "vps1",
+    WEBHOOK_NOTE = "",
+    SHOW_PUBLIC_DISCORD_ID = true,
     SHOW_WEBHOOK_USERNAME = true,
     SHOW_WEBHOOK_JOBID = true,
 }
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/eb9a467b35fe098d20677eb16ec559a4.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c7ac2a2f86ecf894218a424a1be7667.lua"))()
