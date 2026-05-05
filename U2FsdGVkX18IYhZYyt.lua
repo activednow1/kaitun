2d53dc222fb6c2ff36576874d9bc48a0a626320b
activednow1 <activednow1@gmail.com>
2026-05-06 00:49:55 +0700

    Update U2FsdGVkX18IYhZYyt.lua

--- U2FsdGVkX18IYhZYyt.lua
+++ U2FsdGVkX18IYhZYyt.lua
@@ -1,72 +1,99 @@
 script_key="TNKqRPVddeXbUfXarbqmEKcjEsrgfNYh";
-setfpscap(25)
+setfpscap(10)
 
 getgenv().sailorPieceConfig = {  
-    AUTO_UPDATE_RESTART = true,
-    WORLD = "Sea 2",  -- "Sea 1", "Sea 2" (Sea 2 ascension 5+ required)
-    ASCEND_UNTIL_LEVEL = 5,
-    AUTO_CELESTIAL_FAVOR_TITLE = true, -- Auto attack 5000 island bosses
-    AUTO_BOSS_RUSH_AND_INFINITE_TOWER_STAT_BONUS = false,
-    
-    -- Autofarm
-    DO_REPEATABLE_QUEST = "QuestNPC23",
-    -- MULTI_FARM -> Instant tp kill npc
-    MULTI_FARM = { "FastNinja", "StrongBandit", "StrongFighter", "Delinquent", "Bunny", "ArenaFighter", "Ninja", "Swordsman", "AcademyTeacher", "Slime", "StrongSorcerer", "Curse", "Hollow", "Sorcerer", "FrostRogue", "DesertBandit", "Monkey", "Thief" },  
-    -- Ascend/Quest/Weapon Requirements will auto summon + attack
-    AUTO_FARM = { "IceQueenBoss", "Great Mage Boss", "Cosmic Being", "The World Boss", "The World Boss", "Cosmic Being Boss", "TheWorldBoss", "CosmicBeingBoss" },  -- Auto farm ascend/quest/weapon bosses 
-    SUMMON_BOSS = { "Great Mage Normal", "Great Mage Normal", "Ice Queen Normal" },  -- Auto summon ascend/quest/weapon bosses
- 
-    -- Weapons
-    -- Auto detect + equip BEST DPS Weapon + Damage Build for dungeon/inf tower/high hp bosses
-    -- Auto detect + equip BEST AOE Weapon for MULTI_FARM (Fast Farming)
-    -- BUY_WEAPON -> auto spawn/attack required bosses to unlock weapon
-    BUY_WEAPON = {"Katana", "Dark Blade", "Gryphon", "Ichigo", "Strongest In History", "Ice Queen", "The World", "Cosmic Being"},
-    BLESS_WEAPON = { ["Cosmic Being"] = 10, ["The World"] = 10, ["Ice Queen"] = 10, ["Strongest In History"] = 10, ["Ichigo"] = 6, ["Gryphon"] = 6, ["Dark Blade"] = 3 },
+    OPTIMIZATION = true,  -- true = optimize + show ui, false = disable ui
+    AUTO_KICK = true,  -- Autokick if no TRADE_ITEM items left
+    KICK_IF_NO_TRADE_USERNAME = false,
+    TRADE_SEA_1 = true,
+   TRADE_USERNAME = { "petanimaximal36", "petanimaximal37", "petanimaximal38", "petanimaximal39", "petanimaximal40", "petanimaximal41" },  -- "Username"
+    TRADE_ITEM = {
 
-    -- Reroll
-    REROLL_RACE_UNTIL = { "Luckborn" },
-    REROLL_CLAN_UNTIL = { "Devil" },
-    REROLL_TRAIT_UNTIL = { "Emperor" },
-    REROLL_POWER_UNTIL = { "Subjugator" },
-    REROLL_BLOODLINE_UNTIL = { "Primordial" },
-    REROLL_STAT_UNTIL = { 
-        ["Damage"] = "Z", 
-        ["Defense"] = "SS", 
-        ["CooldownReduction"] = "Z", 
-        ["CritChance"] = "SSS", 
-        ["CritDamage"] = "SSS", 
-        ["DamageReduction"] = "SS", 
-        ["Luck"] = "Z" 
-    },
-    REROLL_PASSIVE_UNTIL = { 
-        ["Cosmic Being"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
-        ["The World"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
-        ["Ice Queen"] = { "Fortune Chosen", "Executioner", "Rampage" }, 
-        ["Strongest In History"] = { "Fortune Chosen" }, 
-        ["Ichigo"] = { "Fortune Chosen", "Executioner", "Rampage", "Damage V", "Damage IV" } 
-    },
+    "Aura Crate",
 
-    -- Artifact
-    DELETE_ARTIFACT_RARITY = { "Common" },
-    EQUIP_ARTIFACT_SET = "Abyssal Crown",
+    "Cosmetic Crate",
 
-    -- Misc
-    BUILD_MODE = "Luck",  -- Damage/Luck
-    USE_ITEM = { "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Aura Crate (Untradeable)", "Cosmetic Crate (Untradeable)", "Secret Chest (Untradeable)" },
-    BUY_MERCHANT = { "Race Reroll", "Trait Reroll", "Clan Reroll", "Passive Shard", "Boss Key", "Dungeon Key", "Rush Key", "Boss Ticket", "Haki Color Reroll", "Common Chest", "Rare Chest", "Epic Chest", "Legendary Chest", "Mythical Chest", "Secret Chest" },
+    "Secret Chest",
 
-    -- Autotrade
-    TRADE_USERNAME = {},
-    TRADE_ITEM = {},
-    
-    -- Discord
-    WEBHOOK_ITEM_NAME = { "Aura Crate", "Cosmetic Crate", "Secret Chest" },
-    WEBHOOK_URL = "https://discord.com/api/webhooks/1491133041299689677/gLJSCFrnjwnu_G1o0xnxtRCMDCT9hw0lFxPdT1NLwthFeUvfSJN7lSNCso2h-TwkPB7-",
+    "Clan Reroll",
+
+    "Bloodline Stone",
+
+    "Relic Part #1",
+
+    "Relic Part #2",
+
+    "Relic Part #3",
+
+    "Relic Part #4",
+
+    "Relic Part #5",
+
+    "Relic Part #6",
+
+    "Relic Part #7",
+
+    "Relic Part #8",
+
+    "Frost Relic",
+
+    "Glacier Remnant",
+
+    "Battle Shard",
+
+    "Ice Core",
+
+    "Cosmic Essence",
+
+    "Easter Key",
+
+    "Easter Egg",
+
+    "Void Seed",
+
+    "Monster Pulse",
+
+    "Soul Flame",
+
+    "Mythical Chest",
+
+    "Guild Key",
+
+    "Crystal Key",
+
+    "World Core",
+
+    "Galaxy Shard",
+
+    "Star Mark",
+
+    "Dominion Brand",
+
+    "Time Remnant",
+
+    "Adamantite",
+
+    "Passive Shard",
+
+    "Power Shard",
+
+    "Spell Echo",
+
+    "Ancient Fragment",
+
+    "Mana Core",
+
+    "Magic Essence",
+
+    "Vampire Omen",
+
+},
+
+    WEBHOOK_URL = "",
     DISCORD_ID = "",
-    WEBHOOK_NOTE = "",
-    SHOW_PUBLIC_DISCORD_ID = true,
+    WEBHOOK_NOTE = "vps1",
     SHOW_WEBHOOK_USERNAME = true,
     SHOW_WEBHOOK_JOBID = true,
 }
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c7ac2a2f86ecf894218a424a1be7667.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/eb9a467b35fe098d20677eb16ec559a4.lua"))()
